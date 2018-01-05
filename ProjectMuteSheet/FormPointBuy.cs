8f468a0532586a5fcfdb1b8e2660e5e50a4036c8
Tom <thomas.malcolm.bennett@gmail.com>
2018-01-05 10:20:53 +0000

    Point Method
    Point Buy Method Implmented
    
    Needs fixing as you can get invlaid point scores

--- ProjectMuteSheet/FormPointBuy.cs
+++ ProjectMuteSheet/FormPointBuy.cs
@@ -44,51 +44,15 @@ namespace ProjectMuteSheet
 
         private void btnStrIn_Click(object sender, EventArgs e)
         {
-            int val = Convert.ToInt32(txtStr.Text);
-            val++;
-            txtStr.Text = val.ToString();
-            switch (val)
-            {
-                case 8:
-                    break;
-                case 9:
-                    points -= 1;
-                    break;
-                case 10:
-                    points -= 2;
-                    break;
-                case 11:
-                    points -= 3;
-                    break;
-                case 12:
-                    points -= 4;
-                    break;
-                case 13:
-                    points -= 5;
-                    break;
-                case 14:
-                    points -= 7;
-                    break;
-                case 15:
-                    points -= 9;
-                    break;
-                default:
-                    txtStr.Text = "8";
-                    break;
-            }
-
-            if (points < 0)
-            {
-                points = 27;
-                txtStr.Text = "8";
-}
-            txtBoxPoints.Text = points.ToString();
+            txtStr.Text = PonitsInc(txtStr.Text);
 
         }
 
+        
+
         private void btnStrDe_Click(object sender, EventArgs e)
         {
-            PointsDec(txtStr.Text);
+            txtStr.Text = PointsDec(txtStr.Text);
         }
 
         private string PointsDec(string input)
@@ -143,6 +107,55 @@ namespace ProjectMuteSheet
 
             return newstring;      
         }
+
+        private string PonitsInc(string input)
+        {
+            int val = Convert.ToInt32(input);
+
+            switch (val)
+            {
+                case 8:
+                    break;
+                case 9:
+                    points -= 1;
+                    break;
+                case 10:
+                    points -= 2;
+                    break;
+                case 11:
+                    points -= 3;
+                    break;
+                case 12:
+                    points -= 4;
+                    break;
+                case 13:
+                    points -= 5;
+                    break;
+                case 14:
+                    points -= 7;
+                    break;
+                case 15:
+                    points -= 9;
+                    break;
+                default:
+                    return "8";
+
+            }
+
+            if (points < 0)
+            {
+                points = 27;
+                return "8";
+            }
+            txtBoxPoints.Text = points.ToString();
+
+            val++;
+
+            string newstring = val.ToString();
+
+            return newstring;
+        }
+
     }
 
     }
